33bf290fa34fd8a7b82848a78041c0986e9a8771
darylclaudioo <darylclaudio2805@gmail.com>
2025-01-20 16:45:32 +0700

    Remove Comment

--- main.py
+++ main.py
@@ -4,10 +4,8 @@ from sqlalchemy import create_engine
 from dotenv import load_dotenv
 
 load_dotenv()
-# PostgreSQL connection setup
 POSTGRES_URI = os.getenv("POSTGRES_URI")
 
-# Create database engine
 source_engine = create_engine(POSTGRES_URI)
 
 def extract_data(query, engine):
@@ -19,7 +17,6 @@ def extract_data(query, engine):
         raise
 
 def transform_data(data):
-    # # Drop unnecessary columns or rename columns
     # data = data.drop(columns=["unwanted_column"])
     return data
 
@@ -31,14 +28,11 @@ def load_data_to_csv(data, file_path):
         raise
 
 def main():
-    # Define your SQL query
     sql_query = "SELECT * FROM film ORDER BY film_id"
 
-    # Define the output CSV file path
     output_csv_path = "output_data.csv"
 
     try:
-        # ETL Process
         extracted_data = extract_data(sql_query, source_engine)
         transformed_data = transform_data(extracted_data)
         load_data_to_csv(transformed_data, output_csv_path)
